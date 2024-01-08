82f26e5593d376ef3e0323d82f9d2cf7583ee011
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 21:55:19 +0100

    add tags and cate

--- views/addwiki_view.php
+++ views/addwiki_view.php
@@ -6,16 +6,54 @@
   <div class="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
     <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text">
     <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
-    
-    <!-- icons -->
-    <div class="icons flex text-gray-500 m-2">
-      <svg class="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-      <svg class="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-      <svg class="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" /></svg>
-      <div class="count ml-auto text-gray-400 text-xs font-semibold">0/300</div>
-    </div>
+
+<div class="m-3">
+        <select class="z-2 mt-1 w-full rounded bg-green-100 ring-1 ring-gray-300"  name="tags" required>
+          <option value="" disabled selected>Select Tags</option>
+          <option value="Physics">Physics</option>
+          <option value="Biology">Biology</option>
+          <option value="Chemistry">Chemistry</option>
+          <option value="Literature">Literature</option>
+          <option value="Music">Music</option>
+          <option value="Visual Arts">Visual Arts</option>
+          <option value="Film and Cinema">Film and Cinema</option>
+          <option value="Performing Arts">Performing Arts</option>
+          <option value="World History">World History</option>
+          <option value="Sociology">Sociology</option>
+          <option value="Software">Software</option>
+          <option value="Political Science">Political Science</option>
+          <option value="Economics">Economics</option>
+          <option value="Countries">Countries</option>
+          <option value="Medicine">Medicine</option>
+          <option value="Gaming">Gaming</option>
+          <option value="Nutrition">Nutrition</option>
+          <option value="Fitness">Fitness</option>
+          <option value="Religion">Religion</option>
+          <option value="Spirituality">Spirituality</option>
+          <option value="Ecology">Ecology</option>
+          <option value="Ecology">Cooking</option>
+        </select>
+</div>
+
+<div class="m-3">
+        <select class="z-2 mt-1 w-full rounded bg-blue-100 ring-1 ring-gray-300"  name="category" required>
+          <option value="" disabled selected>Select a category</option>
+          <option value="Science and Technology">Science and Technology</option>
+          <option value="Arts and Culture">Arts and Culture</option>
+          <option value="History and Society">History and Society</option>
+          <option value="Geography and Travel">Geography and Travel</option>
+          <option value="Health and Wellness">Health and Wellness</option>
+          <option value="Nature and Environment">Nature and Environment</option>
+          <option value="Technology and Innovation">Technology and Innovation</option>
+          <option value="Hobbies and Leisure">Hobbies and Leisure</option>
+          <option value="Philosophy and Religion">Philosophy and Religion</option>
+          <option value="Education and Learning">Education and Learning</option>
+          
+        </select>
+</div>
+
     <!-- buttons -->
-    <div class="buttons flex">
+    <div class="buttons flex mt-5">
       <div class="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
       <div class="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
     </div>
