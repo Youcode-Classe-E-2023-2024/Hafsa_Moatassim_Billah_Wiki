576dea64db67b98a88cc47777163bcf270404797
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 21:55:04 +0100

    model page

---
+++ views/about_view.php
@@ -0,0 +1,14 @@
+
+
+
+
+<div  class="container mx-auto flex justify-center items-center px-4 md:px-10 py-20">
+<div class="bg-white dark:bg-gray-800 px-3 md:px-4 py-12 flex flex-col justify-center items-center rounded-xl">
+    <h1 class="mt-8 md:mt-12 text-3xl lg:text-4xl font-semibold leading-10 text-center text-gray-800 text-center md:w-9/12 lg:w-7/12 dark:text-white">Welcome to the coveted family of Wiki</h1>
+    <p class="mt-10 text-base leading-normal text-center text-gray-600 md:w-9/12 lg:w-7/12 dark:text-white">
+    We believe in the power of knowledge to transform lives. Our platform is dedicated to providing a collaborative space where individuals from around the globe can explore,
+    learn, and contribute to a wealth of information. <br>
+    Thank you for being a part of the <b>WIKI</b> community!</p>
+  </div>
+</div>
+
