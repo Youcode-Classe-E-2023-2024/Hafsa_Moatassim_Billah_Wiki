f5cd002e5188293d0d714b34c980d57e048ab5c1
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 21:55:42 +0100

    view

--- views/wiki_view.php
+++ views/wiki_view.php
@@ -1,13 +1,47 @@
 
+
+
 <div class=" bg-indigo-50">
   <header>
     <h1 class="bg-white py-4 text-center">
-    <a href="#" class="text-xl font-bold text-gray-700 cursor-pointer">BUSINESS MODEL CANVAS</h1>
+    <a href="#" class="text-xl font-bold text-gray-700 cursor-pointer">Discover, Learn, and Share with Wiki</h1>
     </h1>
   </header>
   <section class="min-h-screen body-font text-gray-600 ">
     <div class="container mx-auto px-5 py-10">
       <div class="-m-4 flex flex-wrap">
+        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+          <a class="relative block h-48 overflow-hidden rounded">
+            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/crane-nombreuses-parties-differen
+            tes-dessus_698780-1005.jpg?w=740&t=st=1704742240~exp=1704742840~hmac=0397dab293eb70d9ad8284187fe2fc900af66fe96d1ac689878493b5d1a43806" />
+          </a>
+          </a>
+          <div class="mt-4">
+            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
+            <h2 class="title-font text-lg font-medium text-gray-900">The Catalyzer</h2>
+            <p class="mt-1">01/09/2022</p>
+          </div>
+        </div>
+        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+          <a class="relative block h-48 overflow-hidden rounded">
+            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/cuisinier-vue-cote-faisant-delicieuses-pates_23-2150690631.jpg?w=900&t=st=1704742282~exp=1704742882~hmac=4316b58f3033be630e3df64e3996bb9e4c77bf241c8dfaee1ae9a1a5ffae29ec" />
+          </a>
+          <div class="mt-4">
+            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
+            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
+            <p class="mt-1">01/09/2022</p>
+          </div>
+        </div>
+        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+          <a class="relative block h-48 overflow-hidden rounded">
+            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/bustes-grecs-au-soleil_23-2150719340.jpg?w=740&t=st=1704742353~exp=1704742953~hmac=cb54e378de4e8f454e540f6ccb25632860765dda6ab51857da29f66d7ea104b6" />
+          </a>
+          <div class="mt-4">
+            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
+            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
+            <p class="mt-1">01/09/2022</p>
+          </div>
+        </div>
         <div class="w-full p-4 md:w-1/2 lg:w-1/4">
           <a class="relative block h-48 overflow-hidden rounded">
             <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/421x261" />
