0439645822860c710ef2b423efe5bc920e605864
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 21:55:32 +0100

    chnaged the home

--- views/home_view.php
+++ views/home_view.php
@@ -1,92 +1,21 @@
-<!-- navbar -->
-
-   
-
-<!-- content -->
-<div class="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
-<img src="https://picsum.photos/seed/picsum/1900/850" class="absolute top-0 left-0 min-h-full ob" alt="">
-  <div class="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
-    <div class="col-span-6">
-      <span class="uppercase text-white text-xs font-bold mb-2 block">wiki</span>
-      <h1 class="text-white font-extrabold text-5xl mb-8">Knowledge at Your Fingertips: Wiki Edition</h1>
-      <p class="text-stone-100 text-base">
-      Navigating the Landscape of Wisdom, unlocking the Secrets of the World, Embark on a Knowledge Odyssey with Wiki
-      </p>
-      <button class="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button>
-    </div>
-  </div>
-</div>
-
-<!-- <div class="bg-[#f7d0b6] py-20">
-  <div class="max-w-screen-lg mx-auto flex justify-between items-center">
-    <div class="max-w-xl">
-      <h2 class="font-black text-sky-950 text-3xl mb-4">As the leading experts in this field, we're in over 90 countries</h2>
-      <p class="text-base text-sky-950">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-    <button class="text-sky-950 uppercase py-3 text-base px-10 border border-sky-950 hover:bg-sky-950 hover:bg-opacity-10">Get started</button>
-  </div>
-</div>
-<div class="py-12 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-    <div class="w-full flex flex-col items-end pr-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
-      </div>
-    </div>
-
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br>to help you</h2>
-        <p class="text-white text-sm">
-          Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
-        </p>
-        <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="py-4 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+<!-- component -->
+<div class="w-full">
     
-
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#f7d0b6] before:top-0 before:right-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-sky-950 font-black text-5xl leading-snug mb-10">Finpoint is here <br>to help you</h2>
-        <p class="text-sky-950 text-sm">
-          Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
-        </p>
-        <button class="mt-8 text-sky-950 uppercase py-3 text-sm px-10 border border-sky-950 hover:bg-white hover:bg-opacity-10">Talk with expert</button>
-      </div>
+    <div class="flex bg-gray-200" style="height:600px;">
+        <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
+            <div>
+                <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Journey into Wisdom with  <span class="text-indigo-400">Wiki</span></h2>
+                <p class="mt-2 text-sm text-gray-500 md:text-base">Welcome to WIKI! At Wiki, we believe in the power of knowledge to transform lives. Our platform is dedicated to providing a collaborative space where individuals from around the globe can explore, learn, and contribute to a wealth of information.</p>
+                <div class="flex justify-center lg:justify-start mt-6">
+                    <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="index.php?page=login">Get Started</a>
+                    <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="index.php?page=about">Learn More</a>
+                </div>
+            </div>
+        </div>
+        <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
+            <div class="h-full object-cover" style="background-image: url(https://img.freepik.com/vecteurs-libre/lire-livres-encyclopedies-etudiants-qui-etudient-apprennent-evenements-bibliotheque-publique-tutorat-ateliers-gratuits-concept-aide-aux-devoirs-bibliotheque-illustration-isolee-violette-vibrante-lumineuse_335657-442.jpg?w=1060&t=st=1704746813~exp=1704747413~hmac=eb0c5ccc8680e321c8f34d5f84553790f2cd94d0bd88475a04e116eae5a6dced)">
+                <div class="h-full bg-black opacity-25"></div>
+            </div>
+        </div>
     </div>
-    <div class="w-full flex flex-col pl-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
-      </div>
-    </div>
-
-  </div>
 </div>
-
-<div class="py-12 relative overflow-hidden bg-white">
-  <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-    <div class="w-full flex flex-col items-end pr-16">
-      <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
-      <div class="h-full mt-auto overflow-hidden relative">
-        <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt="">
-      </div>
-    </div>
-
-    <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
-      <div class="relative z-20 pl-12">
-        <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here <br>to help you</h2>
-        <p class="text-white text-sm">
-          Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
-        </p>
-        <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
-      </div>
-    </div>
-  </div>
-</div> -->
