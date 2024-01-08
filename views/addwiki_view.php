6f86992f5a13e95268491513aab6f8c0a928463f
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 22:31:16 +0100

    file drag

--- views/addwiki_view.php
+++ views/addwiki_view.php
@@ -4,8 +4,8 @@
   body {background: #eef2ff !important;}
 </style>
   <div class="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-    <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text">
-    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
+    <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" name="title" type="text">
+    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" name="content" placeholder="Describe everything about this post here"></textarea>
 
 <div class="m-3">
         <select class="z-2 mt-1 w-full rounded bg-green-100 ring-1 ring-gray-300"  name="tags" required>
@@ -51,10 +51,23 @@
           
         </select>
 </div>
+    <main class="items-center justify-center bg-gray-100 font-sans">
+      <label for="dropzone-file" class="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+        </svg>
+
+        <h2 class="mt-4 text-xl font-medium text-gray-700 tracking-wide">Wiki File</h2>
+
+        <p class="mt-2 text-gray-500 tracking-wide">Upload or darg & drop your file SVG, PNG, JPG or GIF. </p>
+
+        <input id="dropzone-file" type="file" class="hidden" />
+      </section>
+    </main>
 
     <!-- buttons -->
     <div class="buttons flex mt-5">
       <div class="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
-      <div class="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
+      <div class="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-blue-700">Post</div>
     </div>
   </div>
