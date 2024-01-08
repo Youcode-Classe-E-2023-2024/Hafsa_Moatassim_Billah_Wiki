8bc23913c6debb2bb40815557182cf4222519276
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-08 14:12:32 +0100

    wiki view

---
+++ views/wiki_view.php
@@ -0,0 +1,24 @@
+
+
+<div class=" bg-indigo-50">
+  <header>
+    <h1 class="bg-white py-4 text-center">
+    <a href="#" class="text-xl font-bold text-gray-700 cursor-pointer">BUSINESS MODEL CANVAS</h1>
+    </h1>
+  </header>
+  <section class="min-h-screen body-font text-gray-600 ">
+    <div class="container mx-auto px-5 py-10">
+      <div class="-m-4 flex flex-wrap">
+        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+          <a class="relative block h-48 overflow-hidden rounded">
+            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/421x261" />
+          </a>
+          <div class="mt-4">
+            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
+            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
+            <p class="mt-1">01/09/2022</p>
+          </div>
+        </div>
+    </div>
+  </section>
+</div>
