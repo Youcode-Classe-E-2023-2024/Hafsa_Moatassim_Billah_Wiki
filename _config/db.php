7953e446ffc5a35d0eb99375acaae848eb2d4b14
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-09 19:40:37 +0100

    fix the config

--- _config/db.php
+++ _config/db.php
@@ -1,3 +1,11 @@
 <?php
 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+try {
+    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
