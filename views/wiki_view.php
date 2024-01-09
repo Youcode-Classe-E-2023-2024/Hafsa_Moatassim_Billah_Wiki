63040f90325fe38870b6cee98d60852740712588
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-09 19:39:00 +0100

    display wikis

--- views/wiki_view.php
+++ views/wiki_view.php
@@ -10,72 +10,26 @@
   <section class="min-h-screen body-font text-gray-600 ">
     <div class="container mx-auto px-5 py-10">
       <div class="-m-4 flex flex-wrap">
+        
+        <?php 
+        $id = $_SESSION['c'];
+        $wiki = User::getAllarticles($id);
+
+        foreach($wiki as $article){ ?>
+
         <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/crane-nombreuses-parties-differen
-            tes-dessus_698780-1005.jpg?w=740&t=st=1704742240~exp=1704742840~hmac=0397dab293eb70d9ad8284187fe2fc900af66fe96d1ac689878493b5d1a43806" />
-          </a>
+          <a href="index.php?page=contentwiki&id=<?= $article['id'] ?>" class="relative block h-48 overflow-hidden rounded">
+            <img class="block h-full w-full object-cover object-center cursor-pointer" src="assets/image/<?= $article['file'] ?>" />
           </a>
           <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">The Catalyzer</h2>
-            <p class="mt-1">01/09/2022</p>
+            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500"><?= $article['id_user'] ?></h3>
+            <h2 class="title-font text-lg font-medium text-gray-900"><?= $article['title'] ?></h2>
+            <p class="mt-1"><?= date('F j, Y', strtotime($article['create_at'])) ?></p>
           </div>
         </div>
-        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/cuisinier-vue-cote-faisant-delicieuses-pates_23-2150690631.jpg?w=900&t=st=1704742282~exp=1704742882~hmac=4316b58f3033be630e3df64e3996bb9e4c77bf241c8dfaee1ae9a1a5ffae29ec" />
-          </a>
-          <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
-            <p class="mt-1">01/09/2022</p>
-          </div>
-        </div>
-        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://img.freepik.com/photos-gratuite/bustes-grecs-au-soleil_23-2150719340.jpg?w=740&t=st=1704742353~exp=1704742953~hmac=cb54e378de4e8f454e540f6ccb25632860765dda6ab51857da29f66d7ea104b6" />
-          </a>
-          <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
-            <p class="mt-1">01/09/2022</p>
-          </div>
-        </div>
-        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/421x261" />
-          </a>
-          <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
-            <p class="mt-1">01/09/2022</p>
-          </div>
-        </div>
-        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/421x261" />
-          </a>
-          <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
-            <p class="mt-1">01/09/2022</p>
-          </div>
-        </div>
-        <div class="w-full p-4 md:w-1/2 lg:w-1/4">
-          <a class="relative block h-48 overflow-hidden rounded">
-            <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/421x261" />
-          </a>
-          <div class="mt-4">
-            <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
-            <h2 class="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
-            <p class="mt-1">01/09/2022</p>
-          </div>
-        </div>
+
+        <?php }?>
+  
     </div>
   </section>
-  <!-- Footer -->
-  <footer>
-    <p class="text-center  py-4 bg-white">Create By &copy; YOTHIN INTHAVYXUP</p>
-  </footer>
 </div>
