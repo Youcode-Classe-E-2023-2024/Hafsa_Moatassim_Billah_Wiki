fca958875a8ca7be4d1a2d78963640c2b6431164
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-10 14:39:44 +0100

    add tag

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,2 +1,8 @@
-
-
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $Tag = Tags::create($firstname, $lastname, $email, $password, $file);
+    header("location:index.php?page=dashboard");
+} else {
+    header("location:index.php?page=wiki");
+}
