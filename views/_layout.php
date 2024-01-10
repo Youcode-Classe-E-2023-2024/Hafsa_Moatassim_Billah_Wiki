febaf9d106962a0bf368f96965e4caf32d357d44
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-10 14:40:06 +0100

    layout

--- views/_layout.php
+++ views/_layout.php
@@ -41,6 +41,8 @@
     <script src="<?= PATH ?>assets/js/signup.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
