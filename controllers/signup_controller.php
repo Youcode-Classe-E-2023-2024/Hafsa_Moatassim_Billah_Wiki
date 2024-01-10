851e596b8be97d192a36152af83933f10a8fae64
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-10 12:29:21 +0100

    signup controller

--- controllers/signup_controller.php
+++ controllers/signup_controller.php
@@ -1,19 +1,59 @@
 <?php 
 
-if(isset($_POST['submit']) && isset($_FILES['pp']['name'])) {
+global $db;
+
+if (isset($_POST["req"]) && $_POST["req"] == "submit") {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $file = $_FILES['pp']['name'];
+    
 
+    $file = $_FILES['picture']['name'];
     $uploadDir = 'assets/image/';
-    $originalFileName = $_FILES['pp']['name'];
+    $originalFileName = $_FILES['picture']['name'];
     $uploadFile = $uploadDir . basename($originalFileName);
 
-    move_uploaded_file($_FILES['pp']['tmp_name'], $uploadFile);
-        $User = User::register($firstname, $lastname, $email, $password, $file);
-        header("location:index.php?page=login");
+    move_uploaded_file($_FILES['picture']['tmp_name'], $uploadFile);
 
+    $errors = [
+        "firstName_err" => Validation::validateUsername($firstname),
+        "lastName_err" => Validation::validateUsername($lastname),
+        "email_err" => Validation::validateEmail($email),
+        "password_err" => Validation::validatePassword($password),
+        "userexists_err" => Validation::checkIfUserExist($email, $db),
+    ];
+
+    if (array_filter($errors)) {
+        // Handle errors
+        echo json_encode(["errors" => $errors]);
+        exit;
     }
+
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+    User::register($firstname, $lastname, $email, $passwordHash, $file);
+    echo json_encode(["success" => "User registered successfully"]);
+    exit;
+}
+
+
+
+
+
+// if(isset($_POST['submit']) && isset($_FILES['pp']['name'])) {
+//     $firstname = $_POST['firstname'];
+//     $lastname = $_POST['lastname'];
+//     $email = $_POST['email'];
+//     $password = $_POST['password'];
+//     $file = $_FILES['pp']['name'];
+
+//     $uploadDir = 'assets/image/';
+//     $originalFileName = $_FILES['pp']['name'];
+//     $uploadFile = $uploadDir . basename($originalFileName);
+
+//     move_uploaded_file($_FILES['pp']['tmp_name'], $uploadFile);
+//         $User = User::register($firstname, $lastname, $email, $password, $file);
+//         header("location:index.php?page=login");
+
+//     }
 ?>
