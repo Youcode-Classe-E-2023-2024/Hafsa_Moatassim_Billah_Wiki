8063eeecb0d4276b009a919ae4745159bbe43b80
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-11 18:52:19 +0100

    remove the tags and replace it with loop

--- views/addwiki_view.php
+++ views/addwiki_view.php
@@ -10,46 +10,27 @@
         <textarea class="description bg-gray-100 sec p-3 h-30 border border-gray-300 outline-none"  name="content" placeholder="Describe everything about this post here"></textarea>
 
     <div class="m-3">
+      
             <select class="z-2 mt-1 w-full rounded bg-blue-200 ring-1 ring-gray-300" name="tags[]" required multiple>
               <option value="" disabled selected class="bg-blue-100">Select Tags</option>
-              <option value="Physics">Physics</option>
-              <option value="Biology">Biology</option>
-              <option value="Chemistry">Chemistry</option>
-              <option value="Literature">Literature</option>
-              <option value="Music">Music</option>
-              <option value="Visual Arts">Visual Arts</option>
-              <option value="Film and Cinema">Film and Cinema</option>
-              <option value="Performing Arts">Performing Arts</option>
-              <option value="World History">World History</option>
-              <option value="Sociology">Sociology</option>
-              <option value="Software">Software</option>
-              <option value="Political Science">Political Science</option>
-              <option value="Economics">Economics</option>
-              <option value="Countries">Countries</option>
-              <option value="Medicine">Medicine</option>
-              <option value="Gaming">Gaming</option>
-              <option value="Nutrition">Nutrition</option>
-              <option value="Fitness">Fitness</option>
-              <option value="Religion">Religion</option>
-              <option value="Spirituality">Spirituality</option>
-              <option value="Ecology">Ecology</option>
-              <option value="Ecology">Cooking</option>
+              <?php 
+              $tags = Wiki::getTag();
+              foreach($tags as $tag) {
+              ?>
+              <option value=""><?= $tag['name']?></option>
+            <?php } ?>
             </select>
     </div>
 
     <div class="m-3">
             <select class="z-2 mt-1 w-full rounded bg-blue-100 ring-1 ring-gray-300"  name="category" required>
               <option value="" disabled selected>Select a category</option>
-              <option value="Science and Technology">Science and Technology</option>
-              <option value="Arts and Culture">Arts and Culture</option>
-              <option value="History and Society">History and Society</option>
-              <option value="Geography and Travel">Geography and Travel</option>
-              <option value="Health and Wellness">Health and Wellness</option>
-              <option value="Nature and Environment">Nature and Environment</option>
-              <option value="Technology and Innovation">Technology and Innovation</option>
-              <option value="Hobbies and Leisure">Hobbies and Leisure</option>
-              <option value="Philosophy and Religion">Philosophy and Religion</option>
-              <option value="Education and Learning">Education and Learning</option> 
+              <?php 
+              $cats = Wiki::getCategory();
+              foreach($cats as $cat) {
+              ?>
+              <option value=""><?=$cat['name']?></option>
+              <?php } ?>
             </select>
 
     </div>
