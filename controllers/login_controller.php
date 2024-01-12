1a6cfe2cabdb43e77a9f259b78796c2f394b4017
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-12 16:19:32 +0100

    allow the admin to access the dashboard

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,27 +1,23 @@
 <?php
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
+
     $User = User::login($email, $password);
-    if ($User !== false){
-        
-        $_SESSION['c'] = $User['id'];
-        // $_SESSION['x'] = 'logout';
-       
-        exit(); 
-    } else {
-        header('location: index.php?page=login');
-        echo 'Incorrect password';
-        exit(); 
+    if ($User) {
+
+
+        $_SESSION['id'] = $User['id'];
+        $_SESSION['role'] = $User['role'];
+        $_SESSION['email'] = $User['email'];
+
+        if ($_SESSION['role'] == 'author') {
+            header('location: index.php?page=wiki');
+            exit;
+        } else {
+            header('location: index.php?page=dashboard');
+            exit;
+        }
     }
 }
 
-// if (isset($_GET['logout'])) {
-//     session_destroy();
-//     header('location: index.php?page=home');
-//     exit(); 
-// }
-
-
-
-?>
---
+++ controllers/wiki_controller.php
@@ -0,0 +1,5 @@
+<?php
+
+// var_dump($_SESSION['id']);
+// var_dump($User);
+// var_dump($_SESSION['id']);
