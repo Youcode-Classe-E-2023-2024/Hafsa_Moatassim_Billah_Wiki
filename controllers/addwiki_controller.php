66f73ab667cff0cffe1b09e637eb9c31dc7f6932
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-14 21:35:31 +0100

    random artciles pics

--- controllers/addwiki_controller.php
+++ controllers/addwiki_controller.php
@@ -8,17 +8,30 @@ if(isset($_POST['submit']) && isset($_FILES['img']['name'])) {
     $title = $_POST['title'];
     $content = $_POST['content'];
     $category = $_POST['category'];
-    // $tags = $_POST['tags[]'];
+    $tags = $_POST['tags']; 
+    $id = $_SESSION['id'];
     $file = $_FILES['img']['name'];
+
+    $sanitizedIds = array_map('intval', $tags);
+    $commaSeparatedIds = implode(',', $sanitizedIds);
+
+
+    // dd($tagIdsArray);
 
     $uploadDir = 'assets/image/';
     $originalFileName = $_FILES['img']['name'];
     $uploadFile = $uploadDir . basename($originalFileName);
 
     move_uploaded_file($_FILES['img']['tmp_name'], $uploadFile);
-        $id= $_SESSION['id'];
-        $Wiki = Wiki::NewWiki($title, $content, $file, $category, $id);
-        header("location:index.php?page=wiki");
-
+    
+    $Wiki = Wiki::NewWiki($title, $content, $file, $category, $id);
+    
+    if ($Wiki !== null) {
+        $tags = Wiki::InsertTags($Wiki, $commaSeparatedIds);
+        
+        if ($tags == true) {
+            header("location:index.php?page=wiki");
+        }
     }
+}
 ?>
--- controllers/contentwiki_controller.php
+++ controllers/contentwiki_controller.php
@@ -1,8 +1,19 @@
 <?php 
-        // $id = $_SESSION['id'];
         $ID = $_GET['id'];
 
         $wiki = Wiki::getArticleById($ID);
-    
-        
+
+        $getTagById = Tags::getTag($wiki['id']);
+
+        $tagIdsArray = explode(',', $getTagById[0]['id_tag']);
+        $newTagsArray = [];
+
+         foreach($tagIdsArray as $key => $value){       
+           $getsametags = Tags::getAllTagsById($value);
+
+                foreach ($getsametags as $tag) {
+                array_push($newTagsArray, $tag['name']);
+                }
+        }
+       
 ?>
