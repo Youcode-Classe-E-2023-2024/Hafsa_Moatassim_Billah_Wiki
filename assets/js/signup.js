5a5fd2fedc03a1d5d32937c24c6c55dc99d33d62
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-15 00:03:25 +0100

    validation

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -3,79 +3,29 @@ const signup = document.getElementById('submit');
 
 signup.addEventListener('click', () => {
     console.log("test");
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const file = document.getElementById('picture').value;
-    console.log(file + "file")
-     console.log(firstname);
-     console.log(lastname);
-     console.log(email);
-     console.log(password);
-     console.log(file);
+    const error = document.getElementById('error')
+    const lastname = document.getElementById('lastname_error')
+    const email = document.getElementById('email_error')
+    const password = document.getElementById('password_error')
+    const file = document.getElementById('pic_error')
 
-    var formData = new FormData();
-    formData.append('firstname', firstname);
-    formData.append('lastname', lastname);
-    formData.append('email', email);
-    formData.append('password', password);
-    formData.append('picture', file);
-    formData.append('req', 'submit');
-    console.log(formData);
+    const FormVa = document.getElementById('formvalidation')
 
-    $.ajax({
-        type: "POST",
-        url: "index.php?page=signup",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: (response) => {
-            const data = JSON.parse(response);
-            
-            if (data.success) {
-                alert(data.success);
-            } else if (data.errors) {
-                // console.log(data.errors);
-                var err= data.errors;
-                console.log(err);
-                if (err.firstName_err !== false) {
-                    $("#First_err").text(err.firstName_err);
-                } else {
-                    $("#First_err").text('');
-                }
-            
-                if (err.lastName_err !== false) {
-                    $("#last_err").text(err.lastName_err);
-                } else {
-                    $("#last_err").text('');
-                }
-            
-                if (err.email_err !== false) {
-                    $("#email_err").text(err.email_err);
-                }
-            
-                if (err.userexists_err !== false) {
-                    $("#email_err").text(err.userexists_err);
-                }
-            
-                if (!err.email_err && !err.userexists_err) {
-                    $("#email_err").text('');
-                }
-            
-                if (err.password_err !== false) {
-                    $("#password_err").text(err.password_err);
-                } else {
-                    $("#password_err").text('');
-                }
-                
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = async ()=> {
+        if(req.readyState === XMLHttpRequest.DONE && req.status === 200){
+            let data = JSON.parse(req.response)
+            console.log(data)
+            if(data.success !== ''){
+                location.href = 'index.php?page=login'
+            }else{
+                error.textContent = data.errors
             }
-
-        },
-        error: (error) => {
-            console.log(error);
         }
-    })
-
+    }
+    req.open('POST', 'index.php?page=signup', true)
+    let registerForm = new FormData(FormVa)
+    req.send(registerForm)
+    
     
 });
--- controllers/signup_controller.php
+++ controllers/signup_controller.php
@@ -1,39 +1,46 @@
 <?php 
 
 global $db;
+    
+if(isset($_FILES['picture'])){
 
-if (isset($_POST["req"]) && $_POST["req"] == "submit") {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    
 
     $file = $_FILES['picture']['name'];
     $uploadDir = 'assets/image/';
     $originalFileName = $_FILES['picture']['name'];
     $uploadFile = $uploadDir . basename($originalFileName);
-
     move_uploaded_file($_FILES['picture']['tmp_name'], $uploadFile);
 
-    $errors = [
-        "firstName_err" => Validation::validateFirstname($firstname),
-        "lastName_err" => Validation::validateLastname($lastname),
-        "email_err" => Validation::validateEmail($email),
-        "password_err" => Validation::validatePassword($password),
-        "userexists_err" => Validation::checkIfUserExist($email),
-    ];
+    $imgExtenion = explode('.', $file);
+    $Allowed = strtolower(end($imgExtenion));
+    $Extenion = array('jpg', 'jpeg','png');
 
-    if (array_filter($errors)) {
-        echo json_encode(["errors" => $errors]);
+    if(in_array($Allowed, $Extenion) === true){
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+        User::register($firstname, $lastname, $email, $passwordHash, $file);
+        echo json_encode(["success" => "User registered successfully"]);
         exit;
     }
-
-    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
-    User::register($firstname, $lastname, $email, $passwordHash, $file);
-    echo json_encode(["success" => "User registered successfully"]);
-    exit;
+        $errors = [
+            "firstName_err" => Validation::validateFirstname($firstname),
+            "lastName_err" => Validation::validateLastname($lastname),
+            "email_err" => Validation::validateEmail($email),
+            "password_err" => Validation::validatePassword($password),
+            "userexists_err" => Validation::checkIfUserExist($email),
+            "file_err" =>'you can not upload this type of image',
+            ];
+    
+            if (array_filter($errors)) {
+                echo json_encode(["errors" => $errors]);
+                exit;
+            }
 }
+
+
 
 
 
