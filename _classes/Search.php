18915bbe057193616f42a816a82d994eefaccfa6
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-14 21:34:44 +0100

    search barr

--- _classes/Search.php
+++ _classes/Search.php
@@ -11,16 +11,17 @@ class Search
     static function searchForTitles($title)
     {
         global $db;
-        // % means 0 or more char before or after entered keyword
-        $title = "%" . "$title" . "%";
-        $sql = "SELECT * FROM artciles WHERE title LIKE :title AND archived=0";
+        // % means 0 or more characters before or after the entered keyword
+        $sql = "SELECT * FROM articles WHERE (title LIKE :title AND status = 'published')";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(":title", $title, PDO::PARAM_STR);
+        $title = '%' . $title . '%';  // Add % to the title variable
         $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    
         return $res;
     }
+    
 
     /**
      * @param $tag
@@ -30,18 +31,35 @@ class Search
     static function searchForTags($tag)
     {
         global $db;
-
+    
         // % means 0 or more char before or after entered keyword
         $tag = "%" . "$tag" . "%";
-        $sql = "SELECT tags.*, artciles.* FROM articles_tag
-                JOIN tag ON wiki_tag.tag_id = tag.tag_id
-                JOIN wiki ON wiki_tag.wiki_id = wiki.wiki_id
-                WHERE tag LIKE :tag";
+        $sql = "SELECT tags.*, articles.* FROM articles_tags
+                JOIN tags ON articles_tags.id_tag = tags.id
+                JOIN articles ON articles_tags.id_article = articles.id
+                WHERE tags.name LIKE :tag";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(":tag", $tag, PDO::PARAM_STR);
         $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    
+        return $res;
+    }
+    
+    static function searchForCat($cat)
+    {
+        global $db;
+    
+        // % means 0 or more char before or after entered keyword
+        $cat = "%" . "$cat" . "%";
+        $sql = "SELECT categories.*, articles.* FROM articles
+                JOIN categories ON categories.id = articles.id_category
+                WHERE categories.name LIKE :cat";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":cat", $cat, PDO::PARAM_STR);
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
         return $res;
     }
 }
